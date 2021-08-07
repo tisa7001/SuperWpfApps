7ccb99c660a0a68abd65a57ce658ac38af1a9e72
sachin tiwari <visit2sachin56@gmail.com>
2021-08-07 23:37:14 +0530

    Initial Commits - Toggle

---
+++ SuperWpfApps/HelperClass/ViewBaseModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperWpfApps.HelperClass
+{
+    public class ViewBaseModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        protected bool SetPropertry<T>
+                (ref T Storage, T Value, [CallerMemberName] string Propertyname = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(Storage, Value)) return false;
+            Storage = Value;
+            IPropertyChanged(Propertyname);
+            return true;
+        }
+        protected void IPropertyChanged(string PropertyName)
+        {
+            PropertyChangedEventHandler Handler = this.PropertyChanged;
+            if (Handler != null)
+                Handler(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
--- SuperWpfApps/Model/MainWindowModel.cs
+++ SuperWpfApps/Model/MainWindowModel.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using MahApps.Metro.IconPacks;
 
 namespace SuperWpfApps.Model
 {
     public class MainWindowModel
     {
 
+        /// <summary>
+        /// Update Image on Toggle Button 
+        /// </summary>
+        public void ToggleBtnImageUpdate(ImageSource _togglebtnImageSource)
+        {
+            _togglebtnImageSource = new BitmapImage(new Uri(@"../Assets/arrow - left.png"));
+        }
 
+        
     }
 }
