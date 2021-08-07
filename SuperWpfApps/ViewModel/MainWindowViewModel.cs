b2d971d86e370a75ae1595e6ecac422a91a9f864
sachin tiwari <visit2sachin56@gmail.com>
2021-08-07 22:19:35 +0530

    Add project files.

---
+++ SuperWpfApps/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,77 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SuperWpfApps.ViewModel
+{
+    public class MainWindowViewModel : ViewBaseModel
+    {
+        public System.Windows.Media.ImageSource? _togglebtnImageSource;
+
+
+        public ICommand btnToggleClick
+        {
+            get { return new DelegateCommand<object>(FuncToCall, FuncToEvaluate); }
+        }
+
+        private void FuncToCall(object context)
+        {
+            //this is called when the button is clicked
+
+        }
+
+        private bool FuncToEvaluate(object context)
+        {
+            //this is called to evaluate whether FuncToCall can be called
+            //for example you can return true or false based on some validation logic
+            return true;
+        }
+
+
+        public System.Windows.Media.ImageSource TogglebtnImageSource
+        {
+            get
+            {
+                return _togglebtnImageSource;
+            }
+
+            set
+            {
+                _togglebtnImageSource = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged("TogglebtnImageSource");
+            }
+        }
+
+    }
+
+    public class ViewBaseModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        protected bool SetPropertry<T>
+                (ref T Storage, T Value, [CallerMemberName] string Propertyname = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(Storage, Value)) return false;
+            Storage = Value;
+            IPropertyChanged(Propertyname);
+            return true;
+        }
+        protected void IPropertyChanged(string PropertyName)
+        {
+            PropertyChangedEventHandler Handler = this.PropertyChanged;
+            if (Handler != null)
+                Handler(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
